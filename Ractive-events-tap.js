009a1ac4c0dadda737f291ffd6e04b0fcaf6ba22
Rich Harris <richard.a.harris@gmail.com>
2013-11-16 16:11:58 +0000

    added some inline docs

--- Ractive-events-tap.js
+++ Ractive-events-tap.js
@@ -5,7 +5,22 @@
 
 	Version 0.1.0.
 
-	<< description goes here... >>
+	On mobile devices, using `on-click` isn't good enough. Tapping the
+	touchscreen will fire a simulated click event, but only after a 300
+	millisecond delay, which makes your app feel sluggish. It also
+	causes the tapped area to highlight, which in most cases looks a
+	bit messy.
+
+	Instead, use `on-tap`. When you tap an area, the simulated click
+	event will be prevented, and the user's action is responded to
+	instantly. The `on-tap` event also differs from `on-click` in that
+	the click event will (frankly rather bizarrely) fire even if you
+	hold the mouse down over a single element for several seconds and
+	waggle it about.
+
+	Pointer events are also supported, as is pressing the spacebar when
+	the relevant element is focused (which triggers a click event, and
+	is good for accessibility).
 
 	==========================
 
@@ -26,7 +41,15 @@
 	    // the return value
 	    require( 'Ractive-events-tap' );
 
-	<< more specific instructions for this plugin go here... >>
+	Add a tap event in the normal fashion:
+
+	    <div on-tap='foo'>tap me!</div>
+
+	Then add a handler:
+
+	    ractive.on( 'foo', function ( event ) {
+	      alert( 'tapped' );
+	    });
 
 */
 
