3e7d15c8b57725480cfe07db935782199b95b0f6
Ivan Wills <Ivan.Wills2@optus.com.au>
2018-04-09 08:58:25 +1000

    Debugging events

--- src/ractive-events-tap.js
+++ src/ractive-events-tap.js
@@ -92,16 +92,19 @@ TapHandler.prototype = {
 		};
 
 		if ( window.navigator.pointerEnabled ) {
+            console.log('ractive-events-tap pointerEnabled');
 			this.node.addEventListener( 'pointerup', handleMouseup, false );
 			document.addEventListener( 'pointermove', handleMousemove, false );
 			document.addEventListener( 'pointercancel', cancel, false );
 			this.node.addEventListener( 'click', handleMouseup, false );
 		} else if ( window.navigator.msPointerEnabled ) {
+            console.log('ractive-events-tap msPointerEnabled');
 			this.node.addEventListener( 'MSPointerUp', handleMouseup, false );
 			document.addEventListener( 'MSPointerMove', handleMousemove, false );
 			document.addEventListener( 'MSPointerCancel', cancel, false );
 			this.node.addEventListener( 'click', handleMouseup, false );
 		} else {
+            console.log('ractive-events-tap pointerless');
 			this.node.addEventListener( 'click', handleMouseup, false );
 			document.addEventListener( 'mousemove', handleMousemove, false );
 		}
